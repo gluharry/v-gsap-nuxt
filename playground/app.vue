b10984c1fab795b43f6dd1c213b6a583cdd52737
Simone Franchina <simone@italianonprofit.it>
2025-09-16 10:33:56 +0200

    chore: Add GSAP animations and Container component to playground layout

--- playground/app.vue
+++ playground/app.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="h-100vh flex items-center justify-center">
-    <h1>Ciao</h1>
+  <div class="flex flex-col gap-y-16 p-16 w-[100vw]">
+    <Container>
+      <div v-gsap.from="{ opacity: 0, y: -64 }" class="size-20 bg-red-500"></div>
+    </Container>
+    <Container>
+      <div v-gsap.markers.whenVisible.from="{ opacity: 0, y: 128 }" class="size-20 bg-red-500"></div>
+    </Container>
+    <Container>
+      <div v-gsap.markers.whenVisible.once.from="{ opacity: 0, y: -64 }" class="size-20 bg-red-500"></div>
+    </Container>
+    <Container>
+      <button v-gsap.magnetic>Test</button>
+    </Container>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
---
+++ playground/components/Container.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-[100vh] flex items-center justify-center bg-slate-100 ">
+    <slot/>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
