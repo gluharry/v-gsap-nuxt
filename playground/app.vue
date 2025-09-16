27c8dce26bc0e1cfa24ae1bc7e361af83875612b
Simone Franchina <simone@italianonprofit.it>
2025-09-16 10:53:50 +0200

    chore: Extend GSAP animations in playground and simplify directive usage

--- playground/app.vue
+++ playground/app.vue
@@ -4,13 +4,23 @@
       <div v-gsap.from="{ opacity: 0, y: -64 }" class="size-20 bg-red-500"></div>
     </Container>
     <Container>
-      <div v-gsap.markers.whenVisible.from="{ opacity: 0, y: 128 }" class="size-20 bg-red-500"></div>
+      <div v-gsap.whenVisible.from="{ opacity: 0, y: 128 }" class="size-20 bg-red-500"></div>
     </Container>
     <Container>
-      <div v-gsap.markers.whenVisible.once.from="{ opacity: 0, y: -64 }" class="size-20 bg-red-500"></div>
+      <div v-gsap.whenVisible.once.from="{ opacity: 0, y: -64 }" class="size-20 bg-red-500"></div>
     </Container>
     <Container>
       <button v-gsap.magnetic>Test</button>
+    </Container>
+    <Container>
+      <div v-gsap.timeline.whenVisible.once class="flex p-16 w-full justify-between gap-y-16">
+        <div>
+          <h1 class="text-4xl font-bold"  v-gsap.add.fromInvisible.from="{ x: -32 }"
+              v-gsap.add.to="{ scale: 2 }">Hello World</h1>
+          <div v-gsap.add.from="{ scale: 0 }">They see me animating</div>
+        </div>
+        <div v-gsap.add.withPrevious.from="{ opacity: 0, y: -164 }" class="size-100 bg-red-500"></div>
+      </div>
     </Container>
   </div>
 </template>
