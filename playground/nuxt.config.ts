baf5897c0e0e0f78ec0212506307a37de79c0107
Simone Franchina <simone@italianonprofit.it>
2025-09-16 10:06:54 +0200

    chore: Add Tailwind CSS with Vite plugin integration into playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
+
 export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
   compatibilityDate: '2024-11-28',
-
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   vgsap: {
     presets: [
       {
