febb74edc06070d2ce7610074a76ba917da96a76
Simone Franchina <simone@italianonprofit.it>
2025-10-08 12:23:34 +0200

    chore: Add @nuxt/fonts to devDependencies and define custom font in main.css

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: ['../src/module','@nuxt/fonts'],
   devtools: { enabled: true },
   compatibilityDate: '2024-11-28',
   css: ['~/assets/css/main.css'],
